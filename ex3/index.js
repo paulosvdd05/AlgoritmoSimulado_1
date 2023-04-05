f0e3cdfdd82071fcfac9dd8b923df9e30386a7f5
Paulo Dias <paulosvdd05@gmail.com>
2023-04-05 14:58:40 -0300

    finalizando ex3

--- ex3/index.js
+++ ex3/index.js
@@ -1,10 +1,15 @@
 const calcular = () => {
     let numeros = [
-        (document.getElementById('num1')).value,
-        (document.getElementById('num2')).value
+        parseInt((document.getElementById('num1')).value),
+        parseInt((document.getElementById('num2')).value)
     ]
+    let resultado = 0;
 
     numeros.sort((a,b) => a-b)
 
-    
+    for (let i = numeros[0]; i <= numeros[1]; i++) {
+        resultado += i
+    }
+
+    div.innerHTML = `Resultado = ${resultado}`
 }
