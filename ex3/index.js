b6a8709b93f10b545bba1f8eabf621182f006b38
Paulo Dias <paulosvdd05@gmail.com>
2023-04-05 14:53:48 -0300

    array crescente ex3

---
+++ ex3/index.js
@@ -0,0 +1,10 @@
+const calcular = () => {
+    let numeros = [
+        (document.getElementById('num1')).value,
+        (document.getElementById('num2')).value
+    ]
+
+    numeros.sort((a,b) => a-b)
+
+    
+}
