32a634e2789fbed677efa214895ae33f05d5b662
Paulo Dias <paulosvdd05@gmail.com>
2023-04-05 14:33:06 -0300

    Finalizando ex2

--- ex2/index.js
+++ ex2/index.js
@@ -3,5 +3,5 @@ const calcular = () =>{
     let preco = (document.getElementById('preco')).value
     const promocao = preco*0.5
 
-    div.innerHTML = `${produto} - Valor unitario`
+    div.innerHTML = `${produto} - Valor unitario R$ ${preco}<br>Promoção leve 3 por R$ ${preco*2 + promocao}<br>O 3° produto custa apenas R$ ${promocao}. `
 }
