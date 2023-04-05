6af3362276cb1f9d28169468056e8c1e116db259
Paulo Dias <paulosvdd05@gmail.com>
2023-04-05 14:30:48 -0300

    index ex2

---
+++ ex2/index.js
@@ -0,0 +1,7 @@
+const calcular = () =>{
+    let produto = (document.getElementById('produto')).value
+    let preco = (document.getElementById('preco')).value
+    const promocao = preco*0.5
+
+    div.innerHTML = `${produto} - Valor unitario`
+}
--- ex39/index.js
+++
@@ -1,4 +0,0 @@
-const verificar = () =>{
-    let numero = parseInt((document.getElementById('numero')).value)
-    div.innerHTML = `Salario: R$${numero}<br>Salario com aumento(25%): R$${(numero+(numero*0.25)).toFixed(2)}`
-}
